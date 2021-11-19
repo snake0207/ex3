33a86348349bd3f0643148cf9663c7e5ddc33c03
snake0207 <snake0207@gmail.com>
2021-11-19 17:00:15 +0900

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First Application',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('First app demo'),
+        ),
+        body: Center(
+            child: Column(
+          children: const [
+            Text('Hello'),
+            Text('Hello'),
+          ],
+        )));
+  }
+}
