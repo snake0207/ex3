b6a0dbb5c4d16575ec93fa323c9eccd5412ba49a
이종범 <snake0207@gmail.com>
2021-11-22 20:05:59 +0900

    assets add

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowCheckedModeBanner: false,
       title: 'First Application',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
@@ -21,14 +22,127 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber[500],
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
-        title: Text('First App Demo'),
+        title: Text('Game BBANTO'),
         centerTitle: true,
         backgroundColor: Colors.amber[700],
         elevation: 0.0,
       ),
-      body: Center(),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/bear.jpeg'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              thickness: 0.5,
+              color: Colors.grey[800],
+              endIndent: 30.0,
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text('BBANTO',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'Power Level',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '14',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'text label #1',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'text label #2',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.check_circle_outline),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'text label #3',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/beardoll.jpeg'),
+                radius: 40.0,
+                backgroundColor: Colors.amber[800],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
